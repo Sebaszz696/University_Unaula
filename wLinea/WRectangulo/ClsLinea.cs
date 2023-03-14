7f091ff9562bb24bd7aa6c7a9ea4126597037525
Sebastian Velasquez <124690619+Sebaszz696@users.noreply.github.com>
2023-03-14 14:23:31 -0500

    14 de marzo

---
+++ wLinea/WRectangulo/ClsLinea.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace wLinea
+{
+    class ClsLinea
+    {
+        private int x;
+        private int y;
+        private int xFinal;
+        private int yFinal;
+
+
+        private static int contar = 0;
+
+        public ClsLinea()
+        {
+            x = 0;
+            y = 0;
+            xFinal = 0;
+            yFinal = 0;
+
+        }
+
+        public ClsLinea(int x, int y, int xFinal, int yFinal)
+        {
+            this.x = x;
+            this.y = y;
+            this.xFinal = xFinal;
+            this.yFinal = yFinal;
+        }
+
+        public ClsLinea(double x, double y, double xFinal, double yFinal)
+        {
+            this.x = (int)x;
+            this.y = (int)y;
+            this.xFinal = (int)xFinal;
+            this.yFinal = (int)yFinal;
+        }
+
+        public int obtenX()
+        {
+            return x;
+        }
+        public int obtenXFinal()
+        {
+            return xFinal;
+        }
+
+        public int obtenY()
+        {
+            return y;
+        }
+        public int obtenYFinal()
+        {
+            return yFinal;
+        }
+
+        public void CambiarX(int x)
+        {
+            this.x = x;
+        }
+        public void CambiarY(int y)
+        {
+            this.y = y;
+        }
+        public void CambiarXFinal(int xFinal)
+        {
+            this.xFinal = xFinal;
+        }
+        public void CambiarYFinal(int yFinal)
+        {
+            this.yFinal = yFinal;
+        }
+        public static int obtenCuenta()
+        {
+            contar += 1;
+            return contar;
+        }
+
+
+    }
+}
---
+++ wLinea/WRectangulo/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/// <summary>
+/// Fecha: Marzo 14 de 2023
+/// Autor: Sebastian Velasquez
+/// Descripción: Aplicación de Dibujar una linea
+/// </summary>
+namespace wLinea
+{
+    public partial class frmDibujarLinea : Form
+    {
+
+        public const int ancho = 20;
+        public const int largo = 20;
+        Graphics grafico;
+    
+        public frmDibujarLinea()
+        {
+            InitializeComponent();
+            grafico = CreateGraphics();
+           
+        }
+
+        private void btnDibujarLinea_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double x = double.Parse(txtx.Text);
+                double y = double.Parse(txty.Text);
+
+                double xFinal = double.Parse(txtXfinal.Text);
+                double yFinal = double.Parse(txtYfinal.Text);
+
+                ClsLinea line = new ClsLinea(x, y, xFinal, yFinal);
+
+
+                Pen pen = new Pen(Color.FromArgb(255, 236, 0, 0));
+     
+                grafico.DrawLine(pen, line.obtenX(), line.obtenY(), line.obtenXFinal(), line.obtenYFinal());
+                txtResultado.Text = Convert.ToString(ClsLinea.obtenCuenta());
+
+
+            }
+            catch
+            { 
+
+                MessageBox.Show("Digite un valor de tipo numérico");
+            }
+        }
+
+     
+    }
+}
